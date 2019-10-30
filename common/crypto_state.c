e89986833a3d8a38006dbb1c1a4cef0be6e593ad
willcl-ark <will8clark@gmail.com>
2019-10-30 11:27:30 +0000

    plaintext lightning messages

--- common/crypto_state.c
+++ common/crypto_state.c
@@ -3,20 +3,26 @@
 
 void towire_crypto_state(u8 **ptr, const struct crypto_state *cs)
 {
-	towire_u64(ptr, cs->rn);
-	towire_u64(ptr, cs->sn);
-	towire_secret(ptr, &cs->sk);
-	towire_secret(ptr, &cs->rk);
-	towire_secret(ptr, &cs->s_ck);
-	towire_secret(ptr, &cs->r_ck);
+	towire_bool(ptr, cs->encrypt);
+	if (cs->encrypt) {
+		towire_u64(ptr, cs->rn);
+		towire_u64(ptr, cs->sn);
+		towire_secret(ptr, &cs->sk);
+		towire_secret(ptr, &cs->rk);
+		towire_secret(ptr, &cs->s_ck);
+		towire_secret(ptr, &cs->r_ck);
+	}
 }
 
 void fromwire_crypto_state(const u8 **ptr, size_t *max, struct crypto_state *cs)
 {
-	cs->rn = fromwire_u64(ptr, max);
-	cs->sn = fromwire_u64(ptr, max);
-	fromwire_secret(ptr, max, &cs->sk);
-	fromwire_secret(ptr, max, &cs->rk);
-	fromwire_secret(ptr, max, &cs->s_ck);
-	fromwire_secret(ptr, max, &cs->r_ck);
+	cs->encrypt = fromwire_bool(ptr, max);
+	if (cs->encrypt) {
+		cs->rn = fromwire_u64(ptr, max);
+		cs->sn = fromwire_u64(ptr, max);
+		fromwire_secret(ptr, max, &cs->sk);
+		fromwire_secret(ptr, max, &cs->rk);
+		fromwire_secret(ptr, max, &cs->s_ck);
+		fromwire_secret(ptr, max, &cs->r_ck);
+	}
 }
